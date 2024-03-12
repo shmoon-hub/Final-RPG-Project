b61f385d9b6a164b28500a725a66af626021fbe5
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-12 15:40:31 +0900

    2. navmesh + tutorial map
    전체적으로 튜토리얼 맵구성 + 네비메쉬 에이전트를 이용한 움직임 구현

---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,22 @@
+﻿/////////////////////////////////////////////////
+//   코드 소개                                 //
+//   플레이어의 움직임을 나타내는 코드
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;       // navMeshAgent를 사용하기 위해 import
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
