14eaf0d2ec303dfd31d9c94e14a70d53cc621cff
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-14 16:29:45 +0900

    5. Click to move
    레이캐스트를 이용해서 마우스를 클릭했을때 캐릭터를 이동하게끔 구현함

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -14,17 +14,28 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target; // 이동 타겟 지정
 
-    Ray lastRay; // 레이캐스팅 구현을 위해 레이 타입의 변수 생성
+    // Ray lastRay;  레이캐스팅 구현을 위해 레이 타입의 변수 생성
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0))   // 0은 왼쪽 1은 오른쪽 2는 중간버튼을 의미한다.
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);   // 메인카메라를 lastray 로 가져온다.
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);         // 100을 곱하는 이유는 먼거리까지 Ray가 그려질 것이기 때문이다.
-
-        GetComponent<NavMeshAgent>().destination = target.position;  // 타겟으로 이동하게끔 네비메쉬 설정
     }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   // 메인카메라를 lastray 로 가져온다.
+        RaycastHit hit;       // 충돌 지점을 정보를 담고있는 구조체 (마우스를 클릭했을때)
+        bool hasHit = Physics.Raycast(ray,out hit); //hasHit은 true 혹은 false를 반환
+        if (hasHit == true)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point; // hasHit이 true이면 내비메시 에이전트의 목적지를 레이캐스트 중돌지점으로 변경
+        }
+    }
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);         // 100을 곱하는 이유는 먼거리까지 Ray가 그려질 것이기 때문이다.
+
+         
+    
 }
