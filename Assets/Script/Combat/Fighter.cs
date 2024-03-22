25d325c1f160e990f1f613459888884c76c4653e
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-22 20:23:39 +0900

    20. Enemy Attack Speed adjustment
    적을 공격할때 쉬지않고 계속 공격하는것이 아닌 한번 공격하고 잠깐 쉬었다가 공격하는 식으로 코드를 수정함

--- Assets/Script/Combat/Fighter.cs
+++ Assets/Script/Combat/Fighter.cs
@@ -6,10 +6,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target; 
+        float timeSinceLastAttack = 0;
+
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -26,7 +31,12 @@ namespace RPG.Combat
 
         private void AttackBehavior()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+            
         }
 
         private bool GetIsInRange()
