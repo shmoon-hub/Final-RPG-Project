a4ce36dfff20cf5f6be49c563f26ffa0f215e262
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-22 16:53:08 +0900

    17! keyboard update
    키보드에도 해당 기능이 작동하도록 업데이트

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -8,7 +8,6 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using RPG.Combat;
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
@@ -80,7 +79,7 @@ namespace RPG.Movement
 
 // namespace RPG.Movement
 // {
-//     public class Mover : MonoBehaviour
+//     public class Mover : MonoBehaviour, IAction
 //     {
 //         [SerializeField] Transform target; // 이동 대상 지정
 
@@ -101,7 +100,7 @@ namespace RPG.Movement
 //         public void StartMoveAction(Vector3 destination)
 //         {
 //             GetComponent<ActionScheduler>().StartAction(this);
-//             GetComponent<Fighter>().Cancel(); // Cancel 메소드의 올바른 철자 확인
+//             //GetComponent<Fighter>().Cancel(); // Cancel 메소드의 올바른 철자 확인
 //             MoveTo(destination);
 //         }
 
@@ -111,7 +110,7 @@ namespace RPG.Movement
 //             navMeshAgent.isStopped = false;
 //         }
 
-//         public void Stop()
+//         public void Cancel()
 //         {
 //             navMeshAgent.isStopped = true;
 //         }
