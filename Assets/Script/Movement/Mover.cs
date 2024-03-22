5484cc1ae28715891b40d1e4ba4fc2d899dc5138
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-22 16:27:22 +0900

    16! keyboard update + asset update
    키보드에서도 해당기능이 구현되게끔 수정 + 1단계와 튜토리얼에서 사용할 적 에셋 추가(곰)

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -69,6 +69,7 @@ namespace RPG.Movement
 // using System.Collections;
 // using System.Collections.Generic;
 // using RPG.Combat;
+// using RPG.Core;
 // using UnityEngine;
 // using UnityEngine.AI;
 
@@ -94,6 +95,7 @@ namespace RPG.Movement
 
 //         public void StartMoveAction(Vector3 destination)
 //         {
+//             GetComponent<ActionScheduler>().StartAction(this);
 //             GetComponent<Fighter>().Cancel(); // Cancel 메소드의 올바른 철자 확인
 //             MoveTo(destination);
 //         }
