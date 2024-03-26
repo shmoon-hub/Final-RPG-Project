8570c6af9b8d2cf3750c8a1c65a65daff07c93c0
shmoon-hub <67772272+shmoon-hub@users.noreply.github.com>
2024-03-26 20:10:43 +0900

    34. using Gizmos
    유니티에 있는 기능인 기즈모를 사용하여 적의 추적반경을 표시하고 추적반경의 크기를 조절할수 있게 설정함

--- Assets/Script/Control/AiControllerPeople.cs
+++ Assets/Script/Control/AiControllerPeople.cs
@@ -38,5 +38,12 @@ namespace RPG.Control
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
+         //  유니티에서 제공하는 기즈모 기능 => 상세설명은 unity docs 참조
+        private void OnDrawGizmosSelected() 
+        {
+            Gizmos.color = Color.blue;     // 기즈모에 사용할 색깔을 지정 (파랑색)
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);     // 기즈모에 사용할 모양 설정 및 기즈모 중심 좌표 및 반경 설정
+        }
     }
 }
